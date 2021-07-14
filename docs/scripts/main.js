78e34b3ae966fbc8cd0e550247c5e0fe88149706
Shogo Ogami <sg.tao.so@gmail.com>
2021-07-14 09:17:16 +0900

    temprary update for find points over threshold

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -38,6 +38,21 @@ function init(){
     ctx.font = font;
     ctx.fillText(text, x, y);
   }
+  function filter_loc_array(mat, cols, rows, maxValue, threshold) {
+    let loc_array = [];
+    let _loc = {'id':0, 'col':0, 'row':0 };
+    let filter_val = maxValue > 0 ? maxValue * threshold : 0;
+
+    for (let row = 0; row <= rows; row++) {
+    for (let col = 0; col <= cols; col++) {
+      let _loc_val = mat.floatAt(col, row);
+      if (_loc_val > filter_val) {
+        console.log(` mat (${col},${row}) : ${_loc_val}`);
+      }
+    }}
+  }
+
+
   // 
   function templateMatching() {
     let src = cv.imread('canvasInput');
@@ -48,22 +63,29 @@ function init(){
     // cv.cvtColor(src, src_gray, cv.COLOR_RGBA2GRAY);
     cv.matchTemplate(src, templ, dst, cv.TM_CCOEFF_NORMED, mask);
     let result = cv.minMaxLoc(dst, mask);
-    cv.matchTemplate(src, templ, dst, cv.TM_CCOEFF_NORMED);
+    // cv.matchTemplate(src, templ, dst, cv.TM_CCOEFF_NORMED);
     console.log('dst : ', dst);
+    // console.log('dst depth : ', dst.depth());
+    console.log('dst height, width : ', dst.cols, dst.rows);
     console.log('result : ', result);
     let maxPoint = result.maxLoc;
+    console.log(`dst at maxPoint(${maxPoint.x}, ${maxPoint.y}) = ${dst.floatAt(maxPoint.x, maxPoint.y)}`);
+    // 
+    // filter
+    // filter_loc_array(dst, dst.cols, dst.rows, dst.floatAt(maxPoint.x, maxPoint.y), 0.99);
+    // 
     let color = new cv.Scalar(255, 0, 0, 255);
     let point = new cv.Point(maxPoint.x + templ.cols, maxPoint.y + templ.rows);
     cv.rectangle(src, maxPoint, point, color, 2, cv.LINE_8, 0);
     cv.imshow('canvasOutput', dst);
     cv.imshow('canvasMatching', src);
-    src.delete(); dst.delete(); mask.delete();
+    src.delete(); templ.delete(); dst.delete(); mask.delete();
     // put test on canvas --start--
     canvas_putText('canvasMatching', maxPoint.x, maxPoint.y, "1");
     // put test on canvas -- end --
   }
   // initial dispatch at loading.
-  load_srcImg();
   load_templImg();
+  load_srcImg();
 
 }
